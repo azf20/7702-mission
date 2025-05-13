2a95b86b11ef381ae819bf51154345b7857ee284
azf20 <azfuller20@gmail.com>
2025-05-13 15:21:20 +0100

    docs init

---
+++ packages/docs/README.md
@@ -0,0 +1,3 @@
+# Docs site for 7702
+
+This is a [Vocs](https://vocs.dev) project bootstrapped with the Vocs CLI.
---
+++ packages/docs/vocs.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vocs";
+
+export default defineConfig({
+  title: "Docs",
+  sidebar: [
+    {
+      text: "Example",
+      link: "/example",
+    },
+  ],
+});
